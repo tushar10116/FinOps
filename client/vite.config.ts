340c068471933dc1aac47a9273ac75526bc9dd03
Nandini Jadhav <tushar10116@gmail.com>
2026-07-03 00:57:37 +0530

    vite base changed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base:"/FinOps",
   server: {
     proxy: {
       "/api": "https://finops-nxob.onrender.com/"
