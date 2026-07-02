63c38b39e4c0edf11cbff3fecc16eaf6d9aea084
Nandini Jadhav <tushar10116@gmail.com>
2026-07-03 01:24:53 +0530

    changed routing to hashrouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const queryClient = new QueryClient({defaultOptions:{queries:{refetchOnWindowFocus:false}}});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
