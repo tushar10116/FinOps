373cb32b1180464ad5646182b7e1eb0990c3ce43
Nandini Jadhav <tushar10116@gmail.com>
2026-07-03 00:29:09 +0530

    proxy path added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
-      "/api": "http://127.0.0.1:3001"
+      "/api": "https://finops-nxob.onrender.com/"
     }
   }
 });
